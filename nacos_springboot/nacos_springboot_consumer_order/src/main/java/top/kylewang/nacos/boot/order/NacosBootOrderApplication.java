23d4743ddac0af2a7428a0adc9f1b724d5b6bec2
KyleWang <wk52525@126.com>
2019-03-04 09:14:32 +0800

    init boot code

---
+++ nacos_springboot/nacos_springboot_consumer_order/src/main/java/top/kylewang/nacos/boot/order/NacosBootOrderApplication.java
@@ -0,0 +1,13 @@
+package top.kylewang.nacos.boot.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosBootOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosBootOrderApplication.class, args);
+	}
+
+}
---
+++ nacos_springboot/nacos_springboot_producer_user/src/main/java/top/kylewang/nacos/boot/user/NacosBootUserApplication.java
@@ -0,0 +1,13 @@
+package top.kylewang.nacos.boot.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosBootUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosBootUserApplication.class, args);
+    }
+
+}
